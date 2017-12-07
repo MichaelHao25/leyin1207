275d9777046c24e1e3f4d53f5946351d22dd2866
chenglong <double.check.hao@gmail.com>
2017-12-07 17:34:07 +0800

    complate project

---
+++ js/js.js
@@ -0,0 +1,13 @@
+var mySwiper = new Swiper('.banner .swiper-container', {
+	loop: true,
+	nextButton: '.banner .swiper-button-next',
+	prevButton: '.banner .swiper-button-prev',
+	autoplay: 3000,
+})
+var mySwiper = new Swiper('.quick-browse .swiper-container', {
+	loop: true,
+	nextButton: '.quick-browse .swiper-button-next',
+	prevButton: '.quick-browse .swiper-button-prev',
+	slidesPerView: 5,
+	spaceBetween: 15,
+})
